25b9e201938e991c50e0b4de60535dbf271c056d
gwitfeng <gwitfeng@gmail.com>
2018-09-09 22:00:03 +0800

    final ver.

--- password_retry.py
+++ password_retry.py
@@ -5,8 +5,12 @@ while i > 0 :
     pwd = input('請輸入密碼  : ')
     if pwd == password :
         print ('登入成功 ！')
-        break 
-      
-    else:
+        break      
+    else :
         i = i - 1
-        print ('密碼錯誤,你還有', i ,'次機會輸入') 
+        print ('密碼錯誤 !! ')
+        if i > 0 :
+            print ('你還有', i ,'次機會輸入')
+        else : 
+            print ('不能再輸入密碼,要鎖碼了 ！！')
+              
